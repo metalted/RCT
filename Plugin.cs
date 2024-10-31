49ddc3a93337fbbb69af527b287fd3edf83f17ea
Metalted <metalted@gmail.com>
2024-10-31 06:54:14 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ZeepkistPluginBase
+{
+    [BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string pluginGUID = "com.metalted.zeepkist.rct";
+        public const string pluginName = "RCT";
+        public const string pluginVersion = "1.0";
+
+        public void Awake()
+        {
+            Harmony harmony = new Harmony(pluginGUID);
+            harmony.PatchAll();
+        }
+    }        
+}
